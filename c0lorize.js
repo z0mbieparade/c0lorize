fdbdf27d2da6017be2410455053856a58c5638c0
Nutmeg Anne <z0p@fl0ater.local>
2019-09-16 14:25:32 -0600

    updates for more ease of use

--- c0lorize.js
+++ c0lorize.js
@@ -13,65 +13,77 @@ prompt.get({
 		}
 	}
 }, function (err, result) {
-	if (err) { return onErr(err); }
+	if (err) throw err;
 	var filename = result.file
 
 	var file_arr = filename.split('.');
 	var ext = file_arr.splice(file_arr.length - 1, 1)[0];
 	var name = file_arr.join('.');
 
-	console.log(name, ext)
-
-
 	fs.readFile(filename, 'utf8', function(err, data) {
 		if (err) throw err;
 		console.log('OK: ' + filename);
-		
+		console.log('\x1b[0m', '------------------ START FILE ------------------ ');
 
 		var lines = data.split(/\r?\n/);
 		var new_data = lines.map(format)
 
-		console.log(new_data)
-
 		fs.writeFile(name + '_c0lor.txt', new_data, function(err, data) {
 			if (err) throw err;
+			console.log('\x1b[0m', '------------------ END FILE ------------------ ');
 			console.log('file saved: ' + name + '_c0lor.txt');
 		});
 	});
 });
 
+var cobj = {
+	'\u00030':  { match: ['0', 'white'], term: '\x1b[37m' },
+	'\u00031':  { match: ['1', 'black'], term: '\x1b[30m' },
+	'\u00032':  { match: ['2', 'navy', 'darkblue'], term: '\x1b[34m' },
+	'\u00033':  { match: ['3', 'green', 'darkgreen', 'forest'], term: '\x1b[32m' },
+	'\u00034':  { match: ['4', 'red'], term: '\x1b[91m' },
+	'\u00035':  { match: ['5', 'brown', 'maroon', 'darkred'], term: '\x1b[31m' },
+	'\u00036':  { match: ['6', 'purple', 'violet'], term: '\x1b[35m' },
+	'\u00037':  { match: ['7', 'olive', 'orange'], term: '\x1b[33m' },
+	'\u00038':  { match: ['8', 'yellow'], term: '\x1b[93m' },
+	'\u00039':  { match: ['9', 'lightgreen', 'lime'], term: '\x1b[92m' },
+	'\u000310': { match: ['10', 'teal'], term: '\x1b[36m' },
+	'\u000311': { match: ['11', 'cyan', 'aqua'], term: '\x1b[96m' },
+	'\u000312': { match: ['12', 'blue', 'royal'], term: '\x1b[94m' },
+	'\u000313': { match: ['13', 'pink', 'lightpurple', 'fuchsia'], term: '\x1b[35m' },
+	'\u000314': { match: ['14', 'gray', 'grey'], term: '\x1b[90m' },
+	'\u000315': { match: ['15', 'lightgray', 'lightgrey', 'silver'], term: '\x1b[37m' },
+	'\u001f':   { match: ['underline', 'u'], term: '\x1b[4m' },
+	'\u0016':   { match: ['italic', 'i'], term: '\x1b[3m' },
+	'\u0002':   { match: ['bold', 'b'], term: '\x1b[1m' },
+	'\u000f':   { match: ['reset', 'r'], term: '\x1b[0m' }
+};
+var cobj_keys = Object.keys(cobj);
+
 var format = function(str){
-	var cobj = {
-		'\u00030':  [ '0', 'white'],
-		'\u00031':  [ '1', 'black'],
-		'\u00032':  [ '2', 'navy', 'darkblue'],
-		'\u00033':  [ '3', 'green', 'darkgreen', 'forest'],
-		'\u00034':  [ '4', 'red'],
-		'\u00035':  [ '5', 'brown', 'maroon', 'darkred'],
-		'\u00036':  [ '6', 'purple', 'violet'],
-		'\u00037':  [ '7', 'olive', 'orange'],
-		'\u00038':  [ '8', 'yellow'],
-		'\u00039':  [ '9', 'lightgreen', 'lime'],
-		'\u000310': [ '10', 'teal'],
-		'\u000311': [ '11', 'cyan', 'aqua'],
-		'\u000312': [ '12', 'blue', 'royal'],
-		'\u000313': [ '13', 'pink', 'lightpurple', 'fuchsia'],
-		'\u000314': [ '14', 'gray', 'grey'],
-		'\u000315': [ '15', 'lightgray', 'lightgrey', 'silver'],
-		'\u001f':   ['underline', 'u'],
-		'\u0016':   ['italic', 'i'],
-		'\u0002':   ['bold', 'b'],
-		'\u000f':   ['reset', 'r']
-	};
+	
 
 	var col_count = 0;
-	for(var cid in cobj){
-		for(var i = 0; i < cobj[cid].length; i++){
-			var reg_col = new RegExp('&' + cobj[cid][i], 'g');
-			if(str.match(reg_col) !== null) col_count++;
-			str = str.replace(reg_col, cid);
+	var console_str = str;
+	for(var c = cobj_keys.length - 1; c >= 0; c--) 
+	{ 
+		var cid = cobj_keys[c];
+		for(var i = 0; i < cobj[cid].match.length; i++) 
+		{
+			var reg_col = new RegExp('&' + cobj[cid].match[i], 'g');
+			if(str.match(reg_col) !== null)
+			{
+				col_count++;
+				str = str.replace(reg_col, cid);
+
+				var reg_con = new RegExp('&' + cobj[cid].match[i], 'g');
+				console_str = console_str.replace(reg_con, cobj[cid].term)
+			}
 		}
 	}
+
+	console_str += (col_count > 0 ? '\x1b[0m' : '');
+	console.log(console_str);
 
 	return str + (col_count > 0 ? '\u000f' : '') + '\n';
 }
